5a8136f561fe88b07bcd5fef0bcb15d7cd363818
henglong <907161303@qq.com>
2020-04-16 11:28:16 +0800

    更新说明

---
+++ pages/home/index.js
@@ -0,0 +1,97 @@
+// pages/home/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    logos:[
+    {image: "/image/yungu.png", title: "云股传媒",page:"about"},
+    {image: "/image/3D.png", title: "裸眼3D",page:"luoyan"},
+    {image: "/image/save.png", title: "信息安全",page:'infoSave'},
+    {image: "/image/face.png", title: "人脸识别",page:"face"}],
+    swipers:[{image: "/image/home_one.jpg"},{image: "/image/home_two.jpg"},{image: "/image/home_thr.jpg"}],
+    indicatorDots:true,
+    vertical:false,
+    autoplay: true,
+    interval:3000,
+    duration: 500,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  // 跳转页面
+  showPage(e){
+    console.log(e);
+    let page = e.currentTarget.dataset.page;
+    wx.navigateTo({
+      url: '/pages/'+ page +'/index',
+    })
+  },
+  // 电话联系
+  call(e){
+    let info = e.currentTarget.dataset.info;
+    let phone;
+    if(info == 'business'){
+      phone = '4000-499-984';
+    }else{
+      phone = '020-32547597';
+    }
+    wx.makePhoneCall({
+      phoneNumber: phone,
+    })
+  }
+})
