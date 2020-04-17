bd017cacfcd789272150757405b71fb33d8cac0a
henglong <907161303@qq.com>
2020-04-17 17:47:14 +0800

    更新说明

--- pages/me/index.js
+++ pages/me/index.js
@@ -1,20 +1,70 @@
 // pages/me/index.js
+//获取应用实例
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    loginOut:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true,
+        loginOut:true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+          loginOut:true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true,
+            loginOut:true
+          })
+        }
+      })
+    }
   },
-
+  // 获取用户信息
+  getUserInfo(e){
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true,
+      loginOut:true
+    })
+  },
+  // 退出用户登录
+  loginOut(){
+    app.globalData.userInfo = null;
+    this.setData({
+      loginOut:false
+    })
+    wx.reLaunch({url:"/pages/me/index"})
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -61,6 +111,9 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title:"欢迎分享-云股传媒",
+      path:"/pages/home/index"
+    }
   }
 })
