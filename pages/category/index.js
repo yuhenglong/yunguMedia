95239fb485678f4cad2582a6c5b5fd65f2241788
henglong <907161303@qq.com>
2020-12-10 17:59:03 +0800

    更新说明

--- pages/category/index.js
+++ pages/category/index.js
@@ -8,19 +8,19 @@ Page({
     navLeftItems: [],
     curIndex: 0,
     cityTitle:'广州云股',
-    pointLocation:[{image:'https://www.longjinph.com/test/wx/zgs/guangzhou/1.png',title:'富力海珠城'},{image:'https://www.longjinph.com/test/wx/zgs/guangzhou/2.png',title:'海印又一城'},{image:'https://www.longjinph.com/test/wx/zgs/guangzhou/3.png',title:'五号停机坪'},{image:'https://www.longjinph.com/test/wx/zgs/guangzhou/4.png',title:'万科里'}],
+    pointLocation:[{image:'https://www.oxeyescat.com/test/wx/zgs/guangzhou/1.png',title:'富力海珠城'},{image:'https://www.oxeyescat.com/test/wx/zgs/guangzhou/2.png',title:'海印又一城'},{image:'https://www.oxeyescat.com/test/wx/zgs/guangzhou/3.png',title:'五号停机坪'},{image:'https://www.oxeyescat.com/test/wx/zgs/guangzhou/4.png',title:'万科里'}],
     szTitle:"深圳云股",
-    SZpoint:[{image:'https://www.longjinph.com/test/wx/zgs/shenzhen/1.png',title:'奥卡美广场'},{image:'https://www.longjinph.com/test/wx/zgs/shenzhen/2.png',title:'星城购物中心'},{image:'https://www.longjinph.com/test/wx/zgs/shenzhen/3.png',title:'悦方广场'},{image:'https://www.longjinph.com/test/wx/zgs/shenzhen/4.png',title:'资信广场'}],
+    SZpoint:[{image:'https://www.oxeyescat.com/test/wx/zgs/shenzhen/1.png',title:'奥卡美广场'},{image:'https://www.oxeyescat.com/test/wx/zgs/shenzhen/2.png',title:'星城购物中心'},{image:'https://www.oxeyescat.com/test/wx/zgs/shenzhen/3.png',title:'悦方广场'},{image:'https://www.oxeyescat.com/test/wx/zgs/shenzhen/4.png',title:'资信广场'}],
     cdTitle:"成都云股",
-    CDpoint:[{image:'https://www.longjinph.com/test/wx/zgs/chengdu/1.png',title:'蓝光·东方天地'},{image:'https://www.longjinph.com/test/wx/zgs/chengdu/2.png',title:'融创Nano'},{image:'https://www.longjinph.com/test/wx/zgs/chengdu/3.png',title:'上古天地'},{image:'https://www.longjinph.com/test/wx/zgs/chengdu/4.png',title:'同森金棕榈'}],
+    CDpoint:[{image:'https://www.oxeyescat.com/test/wx/zgs/chengdu/1.png',title:'蓝光·东方天地'},{image:'https://www.oxeyescat.com/test/wx/zgs/chengdu/2.png',title:'融创Nano'},{image:'https://www.oxeyescat.com/test/wx/zgs/chengdu/3.png',title:'上古天地'},{image:'https://www.oxeyescat.com/test/wx/zgs/chengdu/4.png',title:'同森金棕榈'}],
     whTitle:"武汉云股",
-    WHpoint:[{image:'https://www.longjinph.com/test/wx/zgs/wuhan/1.png',title:'福莱中心'},{image:'https://www.longjinph.com/test/wx/zgs/wuhan/2.png',title:'水岸国际'},{image:'https://www.longjinph.com/test/wx/zgs/wuhan/3.png',title:'新世界百货'},{image:'https://www.longjinph.com/test/wx/zgs/wuhan/4.png',title:'印象城'}],
+    WHpoint:[{image:'https://www.oxeyescat.com/test/wx/zgs/wuhan/1.png',title:'福莱中心'},{image:'https://www.oxeyescat.com/test/wx/zgs/wuhan/2.png',title:'水岸国际'},{image:'https://www.oxeyescat.com/test/wx/zgs/wuhan/3.png',title:'新世界百货'},{image:'https://www.oxeyescat.com/test/wx/zgs/wuhan/4.png',title:'印象城'}],
     jmTitle:"江门云股",
-    JMpoint:[{image:'https://www.longjinph.com/test/wx/zgs/jiangmen/1.png',title:'奥园写字楼'},{image:'https://www.longjinph.com/test/wx/zgs/jiangmen/2.png',title:'奥园广场'},{image:'https://www.longjinph.com/test/wx/zgs/jiangmen/3.png',title:'荷塘中嘉广场'},{image:'https://www.longjinph.com/test/wx/zgs/jiangmen/4.png',title:'嘉美影院'}],
+    JMpoint:[{image:'https://www.oxeyescat.com/test/wx/zgs/jiangmen/1.png',title:'奥园写字楼'},{image:'https://www.oxeyescat.com/test/wx/zgs/jiangmen/2.png',title:'奥园广场'},{image:'https://www.oxeyescat.com/test/wx/zgs/jiangmen/3.png',title:'荷塘中嘉广场'},{image:'https://www.oxeyescat.com/test/wx/zgs/jiangmen/4.png',title:'嘉美影院'}],
     lzTitle:"泸州云股",
-    LZpoint:[{image:'https://www.longjinph.com/test/wx/zgs/luzhou/1.png',title:'红星美凯龙商场'},{image:'https://www.longjinph.com/test/wx/zgs/luzhou/2.png',title:'摩尔国际广场'}],
+    LZpoint:[{image:'https://www.oxeyescat.com/test/wx/zgs/luzhou/1.png',title:'红星美凯龙商场'},{image:'https://www.oxeyescat.com/test/wx/zgs/luzhou/2.png',title:'摩尔国际广场'}],
     cqTitle:"重庆云股",
-    CQpoint:[{image:'https://www.longjinph.com/test/wx/zgs/chongqing/1.png',title:'恒大中渝广场'},{image:'https://www.longjinph.com/test/wx/zgs/chongqing/2.png',title:'石桥广场'},{image:'https://www.longjinph.com/test/wx/zgs/chongqing/3.png',title:'奥园广场'},{image:'https://www.longjinph.com/test/wx/zgs/chongqing/4.png',title:'中国人寿大厦'}]
+    CQpoint:[{image:'https://www.oxeyescat.com/test/wx/zgs/chongqing/1.png',title:'恒大中渝广场'},{image:'https://www.oxeyescat.com/test/wx/zgs/chongqing/2.png',title:'石桥广场'},{image:'https://www.oxeyescat.com/test/wx/zgs/chongqing/3.png',title:'奥园广场'},{image:'https://www.oxeyescat.com/test/wx/zgs/chongqing/4.png',title:'中国人寿大厦'}]
   },
   /**
    * 生命周期函数--监听页面加载
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,9 @@
 // pages/news/index.js
 var QQMapWX = require('../../utils/qqmap-wx-jssdk.js');
- 
+
 // 实例化API核心类
 var qqmapsdk = new QQMapWX({
-    key: 'ZTBBZ-OECLJ-6OVFB-KXO6K-CUA57-C6FAZ' // 必填
+  key: 'ZTBBZ-OECLJ-6OVFB-KXO6K-CUA57-C6FAZ' // 必填
 });
 
 Page({
@@ -11,130 +11,127 @@ Page({
    * 页面的初始数据
    */
   data: {
-    backfill:"",
-    scale:14,
-    suggestion:[],
-    showLabelTan:false,
-    population:"5000",
-    equip:"50",
-    longitude:"112.487847",
-    latitude:"38.002607",
-    markers:[{
+    backfill: "",
+    val: "",
+    scale: 14,
+    suggestion: [],
+    showLabelTan: false,
+    population: "5000",
+    equip: "50",
+    longitude: "112.487847",
+    latitude: "38.002607",
+    newAdreObj: {},
+    markers: [{
       iconPath: "/image/location.png",
       id: 0,
       latitude: 38.002607,
       longitude: 105.487847,
-      width: 35,
-      height: 45,
-      joinCluster:true,
-      placeInfo:{
-        population:"100000",
-        equip:"12",
-        placeImg:"/image/yungu.png"
+      joinCluster: true,
+      placeInfo: {
+        population: "100000",
+        equip: "12",
+        placeImg: "/image/yungu.png"
       },
-      label:{
+      label: {
         width: 50,
         height: 30,
         borderWidth: 1,
-        borderColor:"#ffffff",
+        borderColor: "#ffffff",
         borderRadius: 10,
         bgColor: '#ffffff',
-        content:"大傻瓜婷婷"
+        content: "大傻瓜婷婷"
       }
-    },{
+    }, {
       iconPath: "/image/location.png",
       id: 1,
       latitude: 35.002607,
       longitude: 109.487847,
-      width: 40,
-      height: 45,
-      joinCluster:true,
-      placeInfo:{
-        population:"160000",
-        equip:"15",
-        placeImg:"/image/yungu.png"
+      joinCluster: true,
+      placeInfo: {
+        population: "160000",
+        equip: "15",
+        placeImg: "/image/yungu.png"
       },
-      label:{
+      label: {
         width: 50,
         height: 30,
         borderWidth: 1,
-        borderColor:"#ffffff",
+        borderColor: "#ffffff",
         borderRadius: 10,
         bgColor: '#ffffff',
-        content:"萌萌的布布"
+        content: "萌萌的布布"
       }
-    },{
+    }, {
       iconPath: "/image/location.png",
       id: 2,
       latitude: 39.002607,
       longitude: 113.487847,
-      width: 35,
-      height: 45,
-      joinCluster:true,
-      placeInfo:{
-        population:"154000540",
-        equip:"13",
-        placeImg:"/image/yungu.png"
+      joinCluster: true,
+      placeInfo: {
+        population: "154000540",
+        equip: "13",
+        placeImg: "/image/yungu.png"
       },
-      label:{
+      label: {
         width: 50,
         height: 30,
         borderWidth: 1,
-        borderColor:"#ffffff",
+        borderColor: "#ffffff",
         borderRadius: 10,
         bgColor: '#ffffff',
-        content:"傻逼的一二"
+        content: "傻逼的一二"
       }
-    },{
+    }, {
       iconPath: "/image/location.png",
       id: 3,
-      latitude:38.002607,
+      latitude: 38.002607,
       longitude: 112.487847,
-      width: 35,
-      height: 45,
-      joinCluster:true,
-      placeInfo:{
-        population:"200000",
-        equip:"19",
-        placeImg:"/image/yungu.png"
+      joinCluster: true,
+      placeInfo: {
+        population: "200000",
+        equip: "19",
+        placeImg: "/image/yungu.png"
       },
-      label:{
+      label: {
         width: 80,
         height: 30,
         borderWidth: 1,
-        borderColor:"#ffffff",
+        borderColor: "#ffffff",
         borderRadius: 10,
         bgColor: '#ffffff',
-        content:"大傻瓜hah "
+        content: "大傻瓜hah "
       }
     }]
   },
-  labelTan:function (e){
+  labelTan: function (e) {
     const self = this;
     console.log(e.markerId)
     const num = e.markerId;
     const equipment = self.data.markers[num].placeInfo.equip;
     const population = self.data.markers[num].placeInfo.population;
     self.setData({
-      showLabelTan:true,
-      population:population,
-      equip:equipment
+      showLabelTan: true,
+      population: population,
+      equip: equipment
     })
   },
-  bindTap:function(){
+  bindTap: function () {
     this.setData({
-      showLabelTan:false,
+      showLabelTan: false,
     })
   },
-  getAddress:function(e){
+  getAddress: function (e) {
     var _this = this;
     //调用关键词提示接口
     qqmapsdk.getSuggestion({
       //获取输入框值并设置keyword参数
       keyword: e.detail.value, //用户输入的关键词，可设置固定值,如keyword:'KFC'
       region: '广州', //设置城市名，限制关键词所示的地域范围，非必填参数
-      page_size:8,
-      success: function (res) {//搜索成功后的回调
+      page_size: 8,
+      success: function (res) { //搜索成功后的回调
+        _this.setData({
+          val: e.detail.value
+        })
         console.log(res);
         var sug = [];
         for (var i = 0; i < res.data.length; i++) {
@@ -149,80 +146,44 @@ Page({
           });
         }
         _this.setData({ //设置suggestion属性，将关键词搜索结果以列表形式展示
-          suggestion: sug   
+          suggestion: sug
         });
+
       }
     })
   },
-  btnClick:function(e){
+  btnClick: function (e) {
     const self = this;
-      console.log("点击");
-      var id = e.currentTarget.id;
-      for (var i = 0; i < self.data.suggestion.length; i++) {
-        if (i == id) {
-          self.setData({
-            backfill: self.data.suggestion[i].title,
-            latitude: self.data.suggestion[i].latitude,
-            longitude: self.data.suggestion[i].longitude,
-            scale: 18
-          });
-          return;
-        }
+    console.log(this.data.val);
+    qqmapsdk.getSuggestion({
+      keyword: this.data.val,
+      success: function (res) {
+        const arr = [];
+        arr.push({ // 获取返回结果，放到sug数组中
+          title: res.data[0].title,
+          iconPath: "/image/locationRed.png",
+          id: res.data[0].id,
+          addr: res.data[0].address,
+          city: res.data[0].city,
+          district: res.data[0].district,
+          latitude: res.data[0].location.lat,
+          longitude: res.data[0].location.lng
+        });
+        const c = self.data.markers.concat(arr)
+        self.setData({
+          markers: c,
+          backfill: self.data.suggestion[0].title,
+          latitude: self.data.suggestion[0].latitude,
+          longitude: self.data.suggestion[0].longitude,
+          scale: 16
+        })
       }
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
